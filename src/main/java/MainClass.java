3eb29e2210c54f9af19e702ef76c220fec518a93
IgorLevch <igorlevvv@gmail.com>
2023-05-07 21:58:38 +0300

    Commit3

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,5 +5,7 @@ public class MainClass {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"HelloBro","Text1",JOptionPane.WARNING_MESSAGE);
         System.out.println("hiii");
+
+
     }
 }
