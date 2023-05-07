a7c01b84e02fe894a22e3f80ece832d57570b762
IgorLevch <igorlevvv@gmail.com>
2023-05-07 22:12:47 +0300

    Commit 4

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class MainClass {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"HelloBro","Text1",JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null,"The func show Brothers");
         System.out.println("hiii");
     }
 }
