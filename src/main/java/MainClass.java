55222de19e96aba34e627c7c7454366c1fd9e0a6
IgorLevch <igorlevvv@gmail.com>
2023-05-07 21:53:37 +0300

    Commit1

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"HelloBro","Text1",JOptionPane.WARNING_MESSAGE);
+    }
+}
