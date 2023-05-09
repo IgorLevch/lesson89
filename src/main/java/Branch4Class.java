6e096246f85755e399be14c52b4a1fa96d526bf8
IgorLevch <igorlevvv@gmail.com>
2023-05-09 16:47:34 +0300

    Commit6

---
+++ src/main/java/Branch4Class.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Branch4Class {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"4th branch, 6th commit");
+
+    }
+}
